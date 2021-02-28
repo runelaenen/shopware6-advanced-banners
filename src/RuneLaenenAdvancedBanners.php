d7bc9dc0fe9b89d680d26ad26a36be97dd84b8d9
rune.laenen <rune.laenen@intracto.com>
2021-02-28 14:09:09 +0100

    Proper uninstall process

--- src/RuneLaenenAdvancedBanners.php
+++ src/RuneLaenenAdvancedBanners.php
@@ -2,8 +2,59 @@
 
 namespace RuneLaenen\AdvancedBanners;
 
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsAnyFilter;
 use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 
 class RuneLaenenAdvancedBanners extends Plugin
 {
+    public function uninstall(UninstallContext $context): void
+    {
+        parent::uninstall($context);
+
+        if ($context->keepUserData()) {
+            return;
+        }
+
+        // Drop tables
+        /** @var Connection $connection */
+        $connection = $this->container->get(Connection::class);
+        $connection->executeQuery('SET FOREIGN_KEY_CHECKS=0;');
+        $connection->executeQuery('DROP TABLE IF EXISTS `rl_ab_banner`');
+        $connection->executeQuery('DROP TABLE IF EXISTS `rl_ab_banner_translation`');
+
+        // Remove CMS blocks & slots
+        /** @var EntityRepositoryInterface $cmsBlockRepository */
+        $cmsBlockRepository = $this->container->get('cms_block.repository');
+        /** @var EntityRepositoryInterface $cmsSlotRepository */
+        $cmsSlotRepository = $this->container->get('cms_slot.repository');
+
+        $criteria = new Criteria();
+        $criteria->addFilter(new EqualsAnyFilter('type', ['rl-advanced-banner', 'rl-advanced-banner-slider']));
+
+        $cmsSlotRepository->delete(
+            array_values(
+                $cmsSlotRepository->searchIds(
+                    $criteria,
+                    $context->getContext()
+                )->getData()
+            ),
+            $context->getContext()
+        );
+
+        $cmsBlockRepository->delete(
+            array_values(
+                $cmsBlockRepository->searchIds(
+                    $criteria,
+                    $context->getContext()
+                )->getData()
+            ),
+            $context->getContext()
+        );
+
+        $connection->executeQuery('SET FOREIGN_KEY_CHECKS=1;');
+    }
 }
